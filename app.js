0e78dd37e059685791f333aa4b031668b860ea95
Crystal Duong <crystalduong.student@careerdevs.com>
2019-09-05 22:15:03 -0400

    Attributes

--- app.js
+++ app.js
@@ -1,36 +1,3 @@
-// Lesson 13
-const list = document.querySelector('#book-list ul');
-const forms = document.forms;
-
-// delete books
-list.addEventListener('click', (e) => {
-  if(e.target.className == 'delete'){
-    const li = e.target.parentElement;
-    li.parentNode.removeChild(li);
-  }
-});
-
-// add books
-const addForm = forms['add-book'];
-addForm.addEventListener('submit', function(e){
-  e.preventDefault();
-
-  // create elements
-  const value = addForm.querySelector('input[type="text"]').value;
-  const li = document.createElement('li');
-  const bookName = document.createElement('span');
-  const deleteBtn = document.createElement('span');
-
-  // add text content
-  bookName.textContent = value;
-  deleteBtn.textContent = 'delete';
-
-  // add classes
-  bookName.classList.add('name');
-  deleteBtn.classList.add('delete');
-
-  // append to DOM
-  li.appendChild(bookName);
-  li.appendChild(deleteBtn);
-  list.appendChild(li);
-});
+// Lesson 14
+// ¯\_(ツ)_/¯ FIRST RULE OF LESSON 14: YOU DON'T TALK ABOUT LESSON 14.
+// ok but seriously there's no code posted for lesson 14
