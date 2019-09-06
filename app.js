9b2ad41e152e720a56582d118659e0bcbdfbbf74
Crystal Duong <crystalduong.student@careerdevs.com>
2019-09-05 22:17:18 -0400

    Checkboxes & Change Events

--- app.js
+++ app.js
@@ -1,3 +1,46 @@
-// Lesson 14
-// ¯\_(ツ)_/¯ FIRST RULE OF LESSON 14: YOU DON'T TALK ABOUT LESSON 14.
-// ok but seriously there's no code posted for lesson 14
+// Lesson 15
+const list = document.querySelector('#book-list ul');
+const forms = document.forms;
+
+// delete books
+list.addEventListener('click', (e) => {
+  if(e.target.className == 'delete'){
+    const li = e.target.parentElement;
+    li.parentNode.removeChild(li);
+  }
+});
+
+// add books
+const addForm = forms['add-book'];
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
+
+  // create elements
+  const value = addForm.querySelector('input[type="text"]').value;
+  const li = document.createElement('li');
+  const bookName = document.createElement('span');
+  const deleteBtn = document.createElement('span');
+
+  // add text content
+  bookName.textContent = value;
+  deleteBtn.textContent = 'delete';
+
+  // add classes
+  bookName.classList.add('name');
+  deleteBtn.classList.add('delete');
+
+  // append to DOM
+  li.appendChild(bookName);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
+});
+
+// hide books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', function(e){
+  if(hideBox.checked){
+    list.style.display = "none";
+  } else {
+    list.style.display = "initial";
+  }
+});
